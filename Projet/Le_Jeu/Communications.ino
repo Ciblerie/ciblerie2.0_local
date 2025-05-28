0839931a325cf86551f7877fc4f3bd7ea7412f98
Ciblerie-2.0 <family.p2944@gmail.com>
2025-05-28 08:01:38 +0200

    Update Communications.ino

--- Projet/Le_Jeu/Communications.ino
+++ Projet/Le_Jeu/Communications.ino
@@ -1,30 +1,94 @@
 
+// 📨 📥
 void communications() {
   if (Serial1.available() > 0) {
-    char message[32]; // Tableau pour stocker le message reçu
-    int len = Serial1.readBytesUntil('\n', message, 31); // Lire jusqu'à 31 caractères
-    message[len] = '\0'; // Terminer la chaîne de caractères
+    String messageLu = Serial1.readStringUntil('\n');
+    messageLu.trim();
 
-    Serial.print("Message reçu de l'ESP32 : ");
+    char message[32]; // Tableau pour stocker le message reçu
+    messageLu.toCharArray(message, 32);
+    message[messageLu.length()] = '\0';
+
+    Serial.print("📥 Message reçu de l'ESP32 : ");
     Serial.println(message);
 
     // Traitement des messages
-    if (message[0] == 'C' && message[1] == 'F') { // Si le message commence par CF
+    if (message[0] != '\0' && strcmp(message, "START_GAME") != 0 && strcmp(message, "CONFIRMED_GAME") != 0 && strcmp(message, "NEXT_PLAYER") != 0 && strcmp(message, "NEXT_TURN") != 0 && strcmp(message, "FIN_GAME") != 0) {
+      groupeSelectionne = String(message);
       Serial.print("Groupe sélectionné : ");
-      Serial.println(message); // Afficher le message (groupe)
-      //strncpy(groupeSelectionne, message, 3); // Copier le groupe sélectionné // Suppression car groupeSelectionne non utilisé
-      //groupeSelectionne[3] = '\0'; // Terminer la chaîne // Suppression car groupeSelectionne non utilisé
+      Serial.println(groupeSelectionne); // Afficher le message (groupe)
       Serial.print("Groupe enregistré : ");
-      Serial.println(message); // Afficher le groupe // Modifié car groupeSelectionne non utilisé
+      Serial.println(groupeSelectionne); // Afficher le groupe
       envoyerMessage("START"); // Envoyer "START" pour activer le bouton "Attente groupe"
+      //groupeSelectionne = ""; // Réinitialiser la variable groupeSelectionne // Suppression car on doit le faire après
     } else if (strcmp(message, "START_GAME") == 0 && !partieDemarree) {
       Serial.println("Message reçu : START_GAME"); // Log de réception
       Serial.print("Partie démarrée avec le groupe : ");
-      Serial.println(message); // Afficher le groupe sélectionné // Modifié car groupeSelectionne non utilisé
-      //envoyerMessage("START_GAME"); // Ne pas renvoyer START_GAME
-      //CF1(); // Appeler CF1() pour lancer la partie // Suppression car CF1() fait déjà l'envoi de CONFIRMED_GAME
-      envoyerMessage("CONFIRMED_GAME"); // Envoyer "CONFIRMED_GAME" directement
+      Serial.println(groupeSelectionne); // Afficher le groupe sélectionné
+
+      if (groupeSelectionne == "CF1") {
+        CF1();
+      } else if (groupeSelectionne == "CF2") {
+        CF2();
+      } else if (groupeSelectionne == "CF3") {
+        CF3();
+      } else if (groupeSelectionne == "CF4") {
+        CF4();
+      } else if (groupeSelectionne == "CSP1") {
+        CSP1();
+      } else if (groupeSelectionne == "CSP2") {
+        CSP2();
+      } else if (groupeSelectionne == "CSP3") {
+        CSP3();
+      } else if (groupeSelectionne == "CSP4") {
+        CSP4();
+      } else if (groupeSelectionne == "CP1") {
+        CP1();
+      } else if (groupeSelectionne == "CP2") {
+        CP2();
+      } else if (groupeSelectionne == "CP3") {
+        CP3();
+      } else if (groupeSelectionne == "CP4") {
+        CP4();
+      } else if (groupeSelectionne == "CC1") {
+        CC1();
+      } else if (groupeSelectionne == "CC2") {
+        CC2();
+      } else if (groupeSelectionne == "CC3") {
+        CC3();
+      } else if (groupeSelectionne == "CC4") {
+        CC4();
+      } else if (groupeSelectionne == "CL1") {
+        CL1();
+      } else if (groupeSelectionne == "CL2") {
+        CL2();
+      } else if (groupeSelectionne == "CL3") {
+        CL3();
+      } else if (groupeSelectionne == "CL4") {
+        CL4();
+      } else if (groupeSelectionne == "LJ1") {
+        LJ1();
+      } else if (groupeSelectionne == "LJ2") {
+        LJ2();
+      } else if (groupeSelectionne == "LJ3") {
+        LJ3();
+      } else if (groupeSelectionne == "LJ4") {
+        LJ4();
+      } else if (groupeSelectionne == "MS1") {
+        MS1();
+      } else if (groupeSelectionne == "MS2") {
+        MS2();
+      } else if (groupeSelectionne == "MS3") {
+        MS3();
+      } else if (groupeSelectionne == "MS4") {
+        MS4();
+      } else if (groupeSelectionne == "CPT1") {
+        CPT1();
+      } else {
+        Serial.println("Groupe non reconnu !");
+      }
       partieDemarree = true;
+      groupeSelectionne = ""; // Réinitialiser la variable groupeSelectionne // Ajout ici
     } else if (strcmp(message, "CONFIRMED_GAME") == 0 && partieDemarree) {
       Serial.println("Message reçu : CONFIRMED_GAME"); // Log de réception
       envoyerMessage("CONFIRMED_GAME");
@@ -44,7 +108,7 @@ void communications() {
 
 void envoyerMessage(String message) {
   Serial1.println(message);
-  Serial.print("Message envoyé à ESP32 : ");
+  Serial.print("📨 Message envoyé à ESP32 : ");
   Serial.println(message); // Log d'envoi
 }
 
